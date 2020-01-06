195942f8627893f4eb821d0dafa4218305a3fd0f
Abel Van gennep <abelvangennep@wcw-stud-145-109-24-185.wireless.uva.nl>
2020-01-06 15:01:44 +0100

    lezen csv bestand

---
+++ main.py
@@ -0,0 +1,20 @@
+import csv
+
+list_cities = []
+dict_connections = {}
+with open('Data/ConnectiesHolland.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    
+    for row in reader:
+        row.append('0')
+        if row[0] in dict_connections:
+            dict_connections[row[0]].append(row)
+        else:
+            dict_connections[row[0]] = [row]
+        
+        if row[1] in dict_connections:
+            dict_connections[row[1]].append(row)
+        else:
+            dict_connections[row[1]] = [row]
+        
+print(dict_connections)
