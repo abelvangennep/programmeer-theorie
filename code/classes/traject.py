0a5bda77a339eea0be82fc76c7b87a0d7bc984c3
Yana Visscher <yana.visscher@gmail.com>
2020-01-07 13:15:31 +0100

    traject end

--- code/classes/traject.py
+++ code/classes/traject.py
@@ -5,13 +5,14 @@ class Traject():
     def __init__(self): 
         self.connections = [] 
         self.travel_time = 0
+        self.end = False
 
     def add_connection(self, connection): 
         self.connections.append(connection)
         self.travel_time += connection.travel_time
 
     def end_traject(self): 
-        pass
+        self.end = True
 
     def __str__(self): 
         pass
