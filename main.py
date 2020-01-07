a9e494121ac1bc261cca3a63b95e42329a2d0c5b
Abel Van gennep <abelvangennep@wcw-stud-145-109-40-244.wireless.uva.nl>
2020-01-07 09:22:04 +0100

    main geupdate

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import csv
 
-list_cities = []
-dict_connections = {}
-with open('Data/ConnectiesHolland.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile)
-    
-    for row in reader:
-        row.append('0')
-        if row[0] in dict_connections:
-            dict_connections[row[0]].append(row)
-        else:
-            dict_connections[row[0]] = [row]
+def main():
+    list_cities = []
+    dict_connections = {}
+    with open('Data/ConnectiesHolland.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile)
         
-        if row[1] in dict_connections:
-            dict_connections[row[1]].append(row)
-        else:
-            dict_connections[row[1]] = [row]
-        
-for key in dict_connections.keys():
-    dict_connections[key]
+        for row in reader:
+            row.append('0')
+            if row[0] in dict_connections:
+                dict_connections[row[0]].append(row)
+            else:
+                dict_connections[row[0]] = [row]
+            
+            if row[1] in dict_connections:
+                dict_connections[row[1]].append(row)
+            else:
+                dict_connections[row[1]] = [row]
+            
+    for key in dict_connections.keys():
+        dict_connections[key]
+
+if __name__ == "__main__":
+    main()
