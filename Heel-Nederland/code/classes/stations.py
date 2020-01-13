ce4e5ed4a06b8971016607583cda6e42cb50ae27
Emma de Gier <emmadegier@sp-eduroam-220-85.wireless.uva.nl>
2020-01-13 13:33:14 +0100

    heuristieken opgesplitst in verschillende functies

--- Heel-Nederland/code/classes/stations.py
+++ Heel-Nederland/code/classes/stations.py
@@ -6,61 +6,62 @@ class Stations():
     def __init__(self):
         self.stations = {}
 
-    def get_station(self, station_name): 
+    def get_station(self, station_name):
         return self.stations[station_name]
-    
+
 
     def create_station(self, station_name):
-        # print(type(Station(station_name)))
-        if not station_name in self.stations: 
-            self.stations[station_name] = Station(station_name) 
-            
-    def get_random_station(self):        
+        if not station_name in self.stations:
+            self.stations[station_name] = Station(station_name)
+
+    def get_random_station(self):
         unvisited_connections = []
-        stations_with_1_connection = [] 
-        stations_uneven_connections = [] 
-        for station in self.stations.values():
-            # print(station.connections)
-            if len(station.connections) == 1 and station.connections[0].visited == False:
-                stations_with_1_connection.append(station)
+        # stations_with_1_connection = []
+        # stations_uneven_connections = []
+        # for station in self.stations.values():
+        #     if len(station.connections) == 1 and station.connections[0].visited == False:
+        #         stations_with_1_connection.append(station)
                 # stations_with_1_connection.append(station.connections[0].station_2)
 
-            for connection in station.connections:
-                # print(connection.visited)
-                if connection.visited == False:
-                    if connection.station_1 not in unvisited_connections: 
-                        unvisited_connections.append(connection.station_1)
-                    if connection.station_2 not in unvisited_connections: 
-                        unvisited_connections.append(connection.station_2)
-                    # print(connection)
-        if len(stations_with_1_connection) > 0: 
-            print("1")
-            return random.choice(stations_with_1_connection)
-        else:
-            for station in self.stations.values():
-            # print(station.connections)
-                if len(station.connections) % 2 == 1:
-                    for connection in station.connections:
-                        if connection.visited == False and station not in stations_uneven_connections: 
-                            stations_uneven_connections.append(station) 
-
-        if  len(stations_uneven_connections) > 0: 
-            print("2")
-            return random.choice(stations_uneven_connections)
-        print("3")
+        for connection in station.connections:
+            if connection.visited == False:
+                if connection.station_1 not in unvisited_connections:
+                    unvisited_connections.append(connection.station_1)
+                if connection.station_2 not in unvisited_connections:
+                    unvisited_connections.append(connection.station_2)
         return random.choice(unvisited_connections)
 
-    def get_random_station_original(self):        
+    def get_random_station_1_conn(self, uneven):
+        stations_with_1_connection = []
+        for station in self.stations.values():
+            if len(station.connections) == 1 and station.connections[0].visited == False:
+                stations_with_1_connection.append(station)
+        if len(stations_with_1_connection) > 0:
+            return random.choice(stations_with_1_connection)
+        elif uneven:
+            get_random_station_uneven_conn()
+        get_random_station()
+
+    def get_random_station_uneven_conn(self):
+        stations_uneven_connections = []
+        for station in self.stations.values():
+            if len(station.connections) % 2 == 1:
+                for connection in station.connections:
+                    if connection.visited == False and station not in stations_uneven_connections:
+                        stations_uneven_connections.append(station)
+        if  len(stations_uneven_connections) > 0:
+            return random.choice(stations_uneven_connections)
+        get_random_station()
+
+    def get_random_station_original(self):
         unvisited_connections = []
 
         for connections in self.stations.values():
             for connection in connections.connections:
-                # print(connection.visited)
                 if connection.visited == False:
                     unvisited_connections.append(connection.station_1)
                     unvisited_connections.append(connection.station_2)
-                    # print(connection)
-                    
+
         return random.choice(unvisited_connections)
 
     def __str__(self):
@@ -68,4 +69,3 @@ class Stations():
         for station in self.stations:
             stations += station + "\n"
         return stations
-
--- README.md
+++ README.md
@@ -7,3 +7,14 @@ test
 
 
 paniekkk
+
+visualise-----
+pip3 install numpy
+pip3 install git+https://github.com/thearn/pygmaps-extended
+sudo pip3 install gmaps
+
+visualise ------ 2
+pip3 install numpy
+pip3 install pandas
+pip3 install bokeh
+pip3 install geopandas
