195e9c732cd60ed0fd447598300ced20b2843061
Abel Van gennep <abelvangennep@wcw-stud-145-109-21-203.wireless.uva.nl>
2020-01-13 13:41:38 +0100

    prompt user for heuristieken

--- Heel-Nederland/main.py
+++ Heel-Nederland/main.py
@@ -11,11 +11,34 @@ from loaddata import load_data, load_stations, load_connections
 from traject import Traject
 from calculatefunction import calculate
 from cut import cut 
+from stations import Stations
 
 
 if __name__ == '__main__':
     best_score = 0
-    attempts = 100
+    attempts = 1
+
+    # raw_input returns the empty string for "enter"
+    yes = {'yes','y', 'ye', ''}
+    no = {'no','n'}
+
+    # Prompt user for heuristiek: Random station chooses a station with one connection
+    station_1_connection = input("Do you prefer to start with a station, which has only one connection.").lower()
+    if station_1_connection in yes:
+        station_1_connection = True
+    elif station_1_connection in no:
+        station_1_connection = False
+    else:
+        sys.stdout.write("Please respond with 'yes' or 'no'")
+
+    # Prompt user for heuristiek: Random station chooses a station with an uneven number of connections
+    station_uneven_connections = input("Do you prefer to start with a station, with an uneven number of connections.").lower()
+    if station_uneven_connections in yes:
+        station_uneven_connections = True
+    elif station_uneven_connections in no:
+        station_uneven_connections = False
+    else:
+        sys.stdout.write("Please respond with 'yes' or 'no'")
 
     for _ in range(attempts):
         data_list = load_data("data/ConnectiesNationaal.csv")
@@ -34,4 +57,6 @@ if __name__ == '__main__':
     f= open("solution.txt","a+")
     f.write(f"attempts:{attempts}\n" f"SCORE:{best_score}\n\n")
     f.close()
+    
+    
 
