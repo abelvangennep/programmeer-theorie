104c6ec90c57856ba149f71c72a537d68c3d48fc
Yana Visscher <yana.visscher@gmail.com>
2020-01-20 14:47:10 +0100

    extended cut

--- Heel-Nederland/code/algoritmes/cut.py
+++ Heel-Nederland/code/algoritmes/cut.py
@@ -19,5 +19,25 @@ def cut(solution):
             # If the first and last connection can't be deleted, break the while loop 
             else: 
                 break 
-    
+
+    for train in trains:
+        if train.travel_time > 0: 
+            index = -1 
+            counter = 0
+            while counter <= len(train.connections): 
+                index = -1 
+                counter = 1
+                for connection in train.connections: 
+                    index += 1 
+                    if index < len(train.connections) - 1:
+                        if connection == train.connections[index + 1] and connection.visited > 2: 
+                            train.delete_connection(index)
+                            train.delete_connection(index)
+                            index -= 1 
+                            break 
+                        else:
+                            counter += 1
+                    else: 
+                        counter += 1
+
     return solution
