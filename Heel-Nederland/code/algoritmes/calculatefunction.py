ff27ba1147e6fa1950ebe343f58f42a9be82d5e4
Emma de Gier <emmadegier@sp-eduroam-221-85.wireless.uva.nl>
2020-01-24 13:43:10 +0100

    calculate comment

--- Heel-Nederland/code/algoritmes/calculatefunction.py
+++ Heel-Nederland/code/algoritmes/calculatefunction.py
@@ -1,26 +1,21 @@
 def calculate(solution):
-    
+    """Returns the score of the solution"""
+
     trains = solution["trains"]
-    minutes = 0 
+    minutes = 0
     visited_connections = []
-    
-    for train in trains: 
+
+    for train in trains:
         minutes += train.travel_time
 
-        for connection in train.connections: 
-            if connection not in visited_connections: 
+        # Add connection to list if visited
+        for connection in train.connections:
+            if connection not in visited_connections:
                 visited_connections.append(connection)
-                # print(connection)
 
-
-    # print("len(visited_connections)", len(visited_connections))
+    # Calculate variables and score
     P = len(visited_connections) / solution["total_connections"]
-
     T = len(trains)
-
-    # print("P:", P)
-    # print("T", T)
-    # print("minutes", minutes)
     score = P * 10000 - (T * 100 + minutes)
 
-    return score 
+    return score
