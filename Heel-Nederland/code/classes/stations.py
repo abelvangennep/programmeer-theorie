6c954614b44632380eb0cb6567d99b28b091456e
Yana Visscher <yana.visscher@gmail.com>
2020-01-24 14:46:33 +0100

    added comments

--- Heel-Nederland/code/classes/stations.py
+++ Heel-Nederland/code/classes/stations.py
@@ -1,10 +1,12 @@
 from station import Station
 import random
 
+
 class Stations():
     """
     In this class station objects are added to a stations dictionary.
     """
+
     def __init__(self):
         self.stations = {}
 
@@ -21,28 +23,28 @@ class Stations():
         stations_with_1_connection = []
         stations_uneven_connections = []
         unvisited_connections = []
-        all_stations = []
 
         for station in self.stations.values():
-            # If station with one connection
+            # If the heuristic of starting a train with a station that only has
+            # one connection should be applied
             if one_connection:
-                # If one connection and unvisited append
+                # Append the station if it only has one connection
+                # and if it has no visits
                 if len(station.connections) == 1 and station.connections[0].visited == 0:
                     stations_with_1_connection.append(station)
 
             for connection in station.connections:
-                # If station with uneven amount of connections
+                # If the heuristic of starting a train with a station that has 
+                # an uneven amount of connections should be applied
                 if uneven_connection:
+                    # Append the station if it has an uneven amount of connections
+                    # and if it has no visits 
                     if len(station.connections) % 2 == 1 and connection.visited < 1 and station not in stations_uneven_connections:
-                    # HEB VAN DE IF STATEMENT HIERONDER DE VOLGORDE VERANDERD NAAR HIERBOVEN
-                    # if connection.visited < 1 and station not in stations_uneven_connections and len(station.connections) % 2 == 1:
                         stations_uneven_connections.append(station)
 
+                # Append every unvisited station
                 if connection.visited < 1 and station not in unvisited_connections:
                     unvisited_connections.append(station)
-
-            if station not in all_stations:
-                all_stations.append(station)
 
         if len(stations_with_1_connection) > 0:
             return random.choice(stations_with_1_connection)
@@ -53,20 +55,22 @@ class Stations():
         elif len(unvisited_connections) > 0:
             return random.choice(unvisited_connections)
 
-        return random.choice(all_stations)
+        # Return a complete random station, if all the other lists are empty
+        return self.get_complete_random_start_station() 
 
     def get_complete_random_start_station(self):
         """Returns a complete random start station"""
         all_stations = []
 
-        # Add all stations once to list
         for station in self.stations.values():
             if station not in all_stations:
                 all_stations.append(station)
+
         return random.choice(all_stations)
 
     def __str__(self):
         stations = ""
         for station in self.stations:
-            stations += station + "   "
+            stations += station + "  "
+
         return stations
