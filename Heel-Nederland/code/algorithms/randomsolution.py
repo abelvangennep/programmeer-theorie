29404a4afd79f152175266e55e4ccda265b21237
Yana Visscher <yana.visscher@gmail.com>
2020-01-27 14:36:03 +0100

    random solution

--- Heel-Nederland/code/algorithms/randomsolution.py
+++ Heel-Nederland/code/algorithms/randomsolution.py
@@ -1,9 +1,7 @@
 from train import Train
 from stations import Stations
 
-def random_solution(stations_dict, connection_objects, station_1_connection,
-                    station_uneven_connections, station_only_once, max_minutes,
-                    max_trains):
+def random_solution(stations_dict, connection_objects, user_choices):
     """Returns random solution with or without heuristics"""
 
     solution = {}
@@ -11,8 +9,9 @@ def random_solution(stations_dict, connection_objects, station_1_connection,
 
     while True:
         # Get random start station or with heuristics if chosen
-        station = stations_dict.get_random_start_station(station_uneven_connections, \
-                                                         station_1_connection)
+        station = stations_dict.get_random_start_station(\
+            user_choices["station_uneven_connections"], \
+                user_choices["station_1_connection"])
 
         # Make a train object
         train = Train(station)
@@ -21,16 +20,17 @@ def random_solution(stations_dict, connection_objects, station_1_connection,
 
         while True:
             # Get random connection or with heuristics if chosen
-            connection = train.get_random_connection(visited_stations, \
-                                                    station_only_once)
+            connection = train.get_random_connection(visited_stations,\
+                user_choices["station_only_once"])
 
             # Add to visited_stations if heuristic visit_station_only_once is chosen
-            if station_only_once:
+            if user_choices["station_only_once"]:
                 visited_stations.append(connection.station_1)
                 visited_stations.append(connection.station_2)
 
             # Break if maximum minutes is reached
-            if train.travel_time + connection.travel_time > max_minutes:
+            if train.travel_time + connection.travel_time >\
+                user_choices["max_minutes"]:
                 break
 
             train.add_connection(connection)
@@ -45,7 +45,8 @@ def random_solution(stations_dict, connection_objects, station_1_connection,
                 counter_visited += 1
 
         # Break if all connections are visited or max train length is reached
-        if len(connection_objects) == counter_visited or len(trains) > max_trains:
+        if len(connection_objects) == counter_visited or len(trains) >\
+            user_choices["max_trains"]:
             break
 
     # Add trains and connection length to solution dictionary
