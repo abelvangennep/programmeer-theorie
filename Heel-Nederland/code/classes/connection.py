e3ea861aacf41f8ef5d0f213f442a04d6a7e51cc
Yana Visscher <yana.visscher@gmail.com>
2020-01-28 12:39:10 +0100

    connection comments

--- Heel-Nederland/code/classes/connection.py
+++ Heel-Nederland/code/classes/connection.py
@@ -1,8 +1,8 @@
 class Connection():
     """
-    In this class connection objects are created. These connection objects contain
-    the first and second station, the travel time of the connection and the count
-    of how many times the connection is visited.
+    In this class stores connection objects. These connection objects
+    contain the first and second station, the travel time of the connection
+    and the count of how many times the connection is visited.
     """
 
     def __init__(self, station_1, station_2, travel_time):
@@ -13,7 +13,7 @@ class Connection():
 
     def __eq__(self, other):
         """Compare just the two station names"""
-        if self.station_1.name == other.station_1.name and self.station_2.name \
+        if self.station_1.name == other.station_1.name and self.station_2.name\
             == other.station_2.name:
             return True
         return False
@@ -29,4 +29,4 @@ class Connection():
     def __str__(self):
         """Return a string"""
         return f"{self.station_1}, {self.station_2}, {self.travel_time}, \
-                {self.visited}"
+            {self.visited}"
