2b1b5c4b47eeefc18d8c45b424f06df344dfbd30
Abel Van gennep <abelvangennep@wcw-stud-145-109-0-125.wireless.uva.nl>
2020-01-28 14:33:01 +0100

    simulatedannealing bugfix

--- Heel-Nederland/code/algorithms/simulatedannealing.py
+++ Heel-Nederland/code/algorithms/simulatedannealing.py
@@ -10,61 +10,61 @@ import random
 
 def simulated_annealing(solution, stations_dict, user_choices):
     """
-    Simulated annealing is a probabilistic algorithm, which can calculte a
-    global optimum. The algorithm compares the scores of to different 
-    solutions, even a worse score can be excepted. To achieve the optimum,
-    three parameters have to be optimized: the cooling factor, start 
-    temperature and end temperature.
+    Simulated annealing is a probabilistic algorithm, which can calculte a global
+    optimum. The algorithm compares the scores of to different solutions, even a
+    worse score can be excepted. To achieve the optimum, three parameters have
+    to be optimized, the cooling_factor, temperature and end_temperature.
     """
-    score = []
     start = timeit.default_timer()
 
+    score = []
+    
+
     # Set variables with choices user made
-    cooling_factor = user_choices["cooling_factor"]
-    iteration = 0
-    max_minutes = user_choices["max_minutes"]
-    max_trains = user_choices["trains"]
-    temperature_start = user_choices["start_temperature"]
+    max_train_duration = user_choices["max_minutes"]
+    temperature = user_choices["start_temperature"]
     temperature_end = user_choices["end_temperature"]
+    cooling_factor = user_choices["cooling_factor"]
+    amount_of_trains = user_choices["trains"]
+    iteration = 0
     trains = solution["trains"]
 
     # Calculate the difference between the amount of trains in the solution and
     # desired amount of trains
-    number_of_trains_difference = len(solution["trains"]) - max_trains
+    number_of_trains_difference = len(solution["trains"]) - amount_of_trains
 
-    # Delete trains if the difference is bigger than 0 
+    # If the difference is bigger then 0 delete trains
     if number_of_trains_difference > 0:
-        for train in trains[max_trains:]:
+        for train in trains[amount_of_trains:]:
             for connection in train.connections:
                 connection.delete_visit()
-        del solution["trains"][max_trains:]
+        del solution["trains"][amount_of_trains:]
 
-    # Add trains if the difference is smaller than 0
+    # If the difference is smaller then 0 add trains
     elif number_of_trains_difference < 0:
         for _ in range(number_of_trains_difference):
-            station = stations_dict.get_random_station(user_choices\
-                      ["SA_station_uneven_connections"], \
-                      user_choices["SA_station_1_connection"])
+            station = stations_dict.get_complete_random_start_station()
             new_train = Train(station)
 
     # Create a new dictionary solution to compare the old solution with
     solution_temp = {}
     solution_temp["total_connections"] = solution["total_connections"]
 
-    # Iterate until the start temperature is equal to or smaller than the 
-    # desired end temperature
-    while temperature_start > temperature_end:
+    # Iterate untill the temperature is equal or smaller then the desired end
+    while temperature > temperature_end:
         iteration += 1
 
         # Empty temporary solution and choose a random train
         solution_temp["trains"] =  []
         train = random.choice(trains)
 
-        # Append all trains to the temporary solution, except the chosen train
-        # and create a new train
+        # Append al the trains to the temporary solution, except the chosen
+        # train and create a new train
         for every_train in trains:
             if every_train is train:
-                station = stations_dict.get_completely_random_station()
+                station = stations_dict.get_random_start_station(user_choices\
+                        ["SA_station_uneven_connections"]\
+                        , user_choices["SA_station_1_connection"])
                 new_train = Train(station)
                 solution_temp["trains"].append(new_train)
             else:
@@ -73,16 +73,18 @@ def simulated_annealing(solution, stations_dict, user_choices):
         visited_stations = []
 
         while True:
-            # Get a random connection, applying chosen heuristics
+            
+
+            # Get random connection or with heuristics if chosen
             connection = new_train.get_random_connection(visited_stations, \
-                user_choices["SA_station_only_once"])
+                         user_choices["SA_station_only_once"])
 
             if user_choices["SA_station_only_once"]:
                 visited_stations.append(connection.station_1)
                 visited_stations.append(connection.station_2)
 
-            # Break if adding the connection would exceed the max train duration
-            if new_train.travel_time + connection.travel_time > max_minutes:
+            # Break if maximum train duration is reached
+            if new_train.travel_time + connection.travel_time > max_train_duration:
                 break
 
             new_train.add_connection(connection)
@@ -96,7 +98,7 @@ def simulated_annealing(solution, stations_dict, user_choices):
         score.append(K_train_1)
 
         # Change the solution, when new train is accepted
-        if difference > 0 or math.exp(difference / temperature_start) > \
+        if difference > 0 or math.exp(difference / temperature) > \
             random.uniform(0, 1):
 
             for connection in train.connections:
@@ -111,14 +113,16 @@ def simulated_annealing(solution, stations_dict, user_choices):
                 connection.delete_visit()
 
         # Reduce temperature
-        temperature_start = temperature_start * cooling_factor
-
-    # Run visualisation
-    see_annealing(score)
+        temperature = temperature * cooling_factor
 
     # MOET DIT ER NOG UIT VOOR DE INLEVERING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     # Print runtime of simulated annealing
     stop = timeit.default_timer()
     print('Time: ', stop - start)
+    
+    # Run visualisation
+    see_annealing(score)
+
+   
 
     return solution
